6920387dcb485470de37058642a2be1048dd31a8
masoombhabha555 <115450630+masoombhabha555@users.noreply.github.com>
2022-10-17 22:22:47 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,30 +36,30 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['923045610683'] //ur owner number
+global.ownername = "Masoom BhaBha" //ur owner name
+global.ytname = "Masoom BhaBha" //ur yt chanel name
+global.socialm = "GitHub: masoombhabha555" //ur github or insta name
+global.location = "Pakistan, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['923045610683']
+global.ownertag = '923045610683' //ur tag number
+global.botnumber = '923045610683' //Add your Bot Number For Log
+global.botname = 'Masoom BhaBha' //ur bot name
+global.ig = "https://youtu.be/Pgf61nKn30I" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtu.be/Pgf61nKn30I" //ur website to be displayed
+global.sc = 'https://github.com/masoombhabha555' //script link
+global.linkz = 'https://youtu.be/Pgf61nKn30I'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.packname = "MaSoom BhaBha" //ur sticker watermark packname
+global.author = "03045610683" //ur sticker watermark author
+global.footer = "® MaSoom BhaBha"
+global.aliveimg ="https://i.imgur.com/rSk9gRB.jpeg"
+global.allmenuimg ="https://i.imgur.com/rSk9gRB.jpeg"
+global.welcomeimg= "https://i.imgur.com/rSk9gRB.jpeg"
+global.gbyeimg = "https://i.imgur.com/rSk9gRB.jpeg"
 global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
 
 //BOT MEDIAS
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['923045610683'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
